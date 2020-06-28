28dce267d6ab7084a71615ff6cb5face56613680
Vincent Moreschi <vmzcon@gmail.com>
2020-06-28 16:15:24 -0400

    init

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
+    # Flask's development server will automatically serve static files ifmain
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
